6324896f723c1c60fe70000084eea5efce0bdaae
HP <HP@DESKTOP-7C9GN7J>
2026-05-18 09:52:53 +0100

    Ajout de migration pour Postgres

--- Finama.Infrastructure/Migrations/Postgres/20260518085145_AddOtpColumns.Designer.cs
+++ Finama.Infrastructure/Migrations/Postgres/20260518085145_AddOtpColumns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Finama.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Finama.Infrastructure.Migrations.Postgres
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260518085145_AddOtpColumns")]
+    partial class AddOtpColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
